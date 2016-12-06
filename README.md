7ade72078d4e8ca39519c2a7b33a4f436ee73d45
Florian Schliep <florian@appiculous.com>
2016-12-06 19:29:29 +0100

    Implement affiliate redirection, Update ReadMe

--- README.md
+++ README.md
@@ -3,9 +3,13 @@ YOURLS-Amazon-Affiliate
 
 A YOURLS plugin which adds your Amazon Affiliate-Tag to all Amazon URLs before redirection.
 
+##Supported Partner Programs
+This plugin supports `amazon.com`, `amazon.de`, `amazon.co.uk`, `amazon.fr` and `amazon.es` URLs.
+
 ## Installation
 * Open the `plugin.php` file located in the `amazon-affiliate` folder
-* Replace `YOUR_TOKEN_HERE` in **line 14** with your Affiliate Token
+* Replace `YOUR_TOKEN_HERE` in **line 15 to 19** with your respective affiliate tags (each partner program needs a different tag)
+* Replace `YOUR_CAMPAIGN_HERE` with your campain name or assign null
 * Upload the whole `amazon-affiliate` folder to `/user/plugins/` in the YOURLS directory on your server
 * Open your YOURLS admin panel in your browser and navigate to **Manage Plugins**
 * Activate the `Amazon Affiliate` plugin
---
+++ amazon-affiliate/plugin.php
@@ -0,0 +1,103 @@
+<?php
+/*
+Plugin Name: Amazon Affiliate
+Plugin URI: https://github.com/floschliep/YOURLS-Amazon-Affiliate
+Description: Add your Amazon Affiliate-Tag to all Amazon URLs before redirection
+Version: 1.0
+Author: Florian Schliep
+Author URI: https://floschliep.com
+*/
+
+yourls_add_action('pre_redirect', 'flo_amazonAffiliate');
+
+function flo_amazonAffiliate($args) {
+	// insert your personal settings here
+	$tagUS = 'YOUR_TOKEN_HERE';
+	$tagDE = 'YOUR_TOKEN_HERE';
+	$tagUK = 'YOUR_TOKEN_HERE';
+	$tagFR = 'YOUR_TOKEN_HERE';
+	$tagES = 'YOUR_TOKEN_HERE';
+	$campaign = 'YOUR_CAMPAIGN_HERE';
+	
+	// get url from arguments; create dictionary with all regex patterns and their respective affiliate tag as key/value pairs
+	$url = $args[0];
+	$patternTagPairs = array(
+		'/^http(s)?:\\/\\/(www\\.)?amazon.com+/ui' => $tagUS,
+		'/^http(s)?:\\/\\/(www\\.)?amazon.de+/ui' => $tagDE,
+		'/^http(s)?:\\/\\/(www\\.)?amazon.co.uk+/ui' => $tagUK,
+		'/^http(s)?:\\/\\/(www\\.)?amazon.fr+/ui' => $tagFR,
+		'/^http(s)?:\\/\\/(www\\.)?amazon.es+/ui' => $tagES,
+	);
+	
+	// check if URL is a supported Amazon URL
+	foreach ($patternTagPairs as $pattern => $tag) {
+		if (preg_match($pattern, $url) == true) {
+			// matched URL, now modify URL
+			$url = cleanUpURL($url);
+			$url = addTagToURL($url, $tag);
+			$url = addCampaignToURL($url, $campaign);
+
+			// redirect
+			header("HTTP/1.1 301 Moved Permanently");
+			header("Location: $url");
+			
+			// now die so the normal flow of event is interrupted
+			die();
+		}
+	}
+}
+
+function cleanUpURL($url) {
+	// check if last char is an "/" (in case it is, remove it)
+	if (substr($url, -1) == "/") {
+		$url = substr($url, 0, -1);
+	}
+	
+	// remove existing affiliate tag if needed
+	$existingTag;
+	if (preg_match('/tag=.+&?/ui', $url, $matches) == true) {
+		$existingTag = $matches[0]; 
+	}
+	if ($existingTag) {
+		$url = str_replace($existingTag, "", $url);
+	}
+	
+	// remove existing campaign if needed
+	$existingCampagin;
+	if (preg_match('/camp=.+&?/ui', $url, $matches) == true) {
+		$existingCampagin = $matches[0]; 
+	}
+	if ($existingCampagin) {
+		$url = str_replace($existingCampagin, "", $url);
+	}
+	
+	return $url;
+}
+
+function addTagToURL($url, $tag) {
+	// add our tag to the URL
+	if (strpos($url, '?') !== false) { 
+		// there's already a query string in our URL, so add our tag with "&"
+		// add token depending on if we need to add a "&" or not
+		if (substr($url, -1) == "&") {
+			$url = $url.'tag='.$tag;
+		} else {
+			$url = $url.'&tag='.$tag;
+		}
+	} else { // start a new query string
+		$url = $url.'?tag='.$tag;
+	}
+
+	return $url;
+}
+
+function addCampaignToURL($url, $campaign) {
+	if (empty($campaign)) {
+		return $url;
+	}
+	$url = $url.'&camp='.$campaign;
+	
+	return $url;
+}
+
+?>
